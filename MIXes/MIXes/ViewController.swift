96c98025c07259bb2d0d6ddb3b1aeda55939d739
ter127 <74223273+ter127@users.noreply.github.com>
2021-10-09 15:43:21 +0900

    xcode 프로젝트 생성

---
+++ MIXes/MIXes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MIXes
+//
+//  Created by 김결 on 2021/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
